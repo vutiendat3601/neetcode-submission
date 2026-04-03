eb0087f157b05e8595c9db8ae41c78fad0c118c7
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-04-03 10:27:39 +0700

    Add: n-ary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/n-ary-tree-postorder-traversal/submission-0.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+private:
+    vector<int> ans;
+    void traversal(Node* root) {
+        if (!root) {
+            return;
+        }
+        for (auto & node : root->children) {
+            traversal(node);
+        }
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorder(Node* root) {
+        traversal(root);
+        return ans;
+    }
+};
