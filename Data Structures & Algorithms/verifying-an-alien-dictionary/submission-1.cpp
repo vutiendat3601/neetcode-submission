516d19ea9a16dc494d0699abc5e5f84427bf913d
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-04-14 11:38:39 +0700

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> idx;
+        for (int i = 0; i < order.size(); i++)
+            idx[order[i]] = i;
+        for (int i = 0; i < words.size() - 1; i++) {
+            int j = 0;
+            string a = words[i], b = words[i + 1];
+            if (a.starts_with(b))
+                return 0;
+            while (j < a.size() && j < b.size()) {
+                if (idx[a[j]] < idx[b[j]])
+                    break;
+                if (idx[a[j]] > idx[b[j]])
+                    return 0;
+                j++;
+            }
+        }
+        return 1;
+    }
+};
