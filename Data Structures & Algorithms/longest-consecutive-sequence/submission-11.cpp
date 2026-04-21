35b248b80c5dd9c3399b1b16d1c70f12ae2fdc99
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-04-21 13:27:09 +0700

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int ans = 0;
+        for (auto& num : nums) {
+            if (!mp[num]) {
+                mp[num] = mp[num - 1] + mp[num + 1] + 1;
+                mp[num - mp[num - 1]] = mp[num + mp[num + 1]] = mp[num];
+                ans = max(ans, mp[num]);
+            }
+        }
+        return ans;
+    }
+};
+
+// HashMap (from a num to 2 side boundary)
