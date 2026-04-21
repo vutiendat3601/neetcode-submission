a198305b8e67fa23d9199f33a430f33e38e8359f
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-04-21 11:32:29 +0700

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    vector<int> parent;
+    int find(int u) {
+        return u == parent[u] ? u : (parent[u] = find(parent[u]));
+    }
+
+    bool unite(int u, int v) {
+        u = find(u), v = find(v), parent[v] = u;
+        return u == v;
+    }
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        parent.resize(n + 1), iota(parent.begin(), parent.end(), 0);
+        for (auto& e : edges)
+            if (unite(e[0], e[1]))
+                return e;
+        return {};
+    }
+};
