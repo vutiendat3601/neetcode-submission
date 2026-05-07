0b0398ec4ef0d93fccf4bfeba48ab136a2e0409c
Dat Vu <64895840+vutiendat3601@users.noreply.github.com>
2026-05-07 16:57:05 +0700

    Add: minimum-cost-for-tickets - submission-2

---
+++ Data Structures & Algorithms/minimum-cost-for-tickets/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        vector<int> dp(n, INT_MAX);
+        for (int i = 0; i < n; i++) {
+            vector<int> prev_days = {days[i] - 1, days[i] - 7, days[i] - 30};
+            for (int j = 0; j < 3; j++) {
+                int k = i;
+                while (k >= 0 && days[k] > prev_days[j]) k--;
+                dp[i] = min(dp[i], costs[j] + (k >= 0 ? dp[k] : 0));
+            }
+        }
+        return dp[n - 1];
+    }
+};
+
+// DP, time: O(n), space: O(n)
